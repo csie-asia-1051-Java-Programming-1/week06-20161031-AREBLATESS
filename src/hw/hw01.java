56b5aba9d1054bcf1af85f0a7b65a8debb036749
AREBLATESS <105021008@live.asia.edu.tw>
2016-11-04 00:10:57 +0800

    105021008 陳畯程 hw01-02

--- src/hw/hw01.java
+++ src/hw/hw01.java
@@ -1,9 +1,29 @@
 package hw;
-
+//請設計一個函數fun(v1, type)，讓使用者輸入一個溫度值及計算方式
+//type為1時華氏->攝氏；type為2時攝氏->華氏
+//函數算出結果後要回傳至主程式再印出結果。(F=C*9/5+32), (C = (F-32) * (5/9)例如:
+//輸入 100 1 則輸出37.77778
+//輸入 37.77778 2 時則輸出 100.0
+import java.util.Scanner;
 public class hw01 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Scanner scn=new Scanner(System.in);
+		double v1=scn.nextDouble();
+		int type=scn.nextInt();
+		fun1(v1,type);}
+	public static void fun1(double v1,int type){
+		double v2=0;
+		switch(type){
+		case 1: //v1華式 v2攝氏
+			v2 = (v1-32) * 5/9;
+			System.out.print(v2+"攝氏");
+			break;
+		case 2: //v1攝氏 v2華氏 
+			v2=v1*9/5+32;
+			System.out.print((float)v2+"華氏");
+			break;}
 
 	}
 
--- src/hw/hw02.java
+++ src/hw/hw02.java
@@ -1,10 +1,33 @@
 package hw;
-
+//讓使用者輸入性別與身高，透過函數呼叫，依據男女不同, 幫她(他)計算並輸出其標準體重
+//((1)男：(身高-170)0.6+62 , (2)女：(身高-158)0.5+52)。
+//函數算出結果後要回傳至主程式再印出結果。例如:
+//輸入 1 170 則輸出 62.0
+//輸入 2 165 則輸出 55.5
+import java.util.Scanner;
 public class hw02 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Scanner scn=new Scanner(System.in);
+		int type=scn.nextInt();
+		double h=scn.nextFloat();
+		System.out.print(fun1(type,h));}
+	
+	public static double fun1(int type,double h){
+		double w=0;
+		switch(type){
+		case 1:
+			w=(h-170)*0.6+62;
+			
+			break;
+		case 2:
+			w=(h-158)*0.5+52;
+			
+			break;
+		}
+		return w;
+		
 	}
 
 }
